073636d08ef04b3ba3c8619edcb4eeb9dfa29246
Brian Obot <brianobot@gmail.com>
2025-11-16 21:23:08 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,6 +8,65 @@ from pathlib import Path
 BUILTIN_CMDS = {"echo", "exit", "type", "pwd", "cd"}
 
 
+def cd(directory: str):
+    if directory.startswith("~"):
+        directory = directory.replace("~", os.environ["HOME"])
+    try:
+        os.chdir(directory)
+    except FileNotFoundError:
+        print(f"cd: {directory}: No such file or directory")
+    
+        
+def echo(user_input: str):
+    text = user_input.lstrip("echo").lstrip()
+    print(parse_quoted_str(text))       
+
+
+def _type(command: str):
+    if command in BUILTIN_CMDS:
+        print(f"{command} is a shell builtin")
+        return 
+        
+    # TODO: Dirty patch until i can figure out why path is corrupted for MG5 Tets
+    if command in ["cat", "cp", "mkdir", "my_exe"]:
+        _is_exec: str | None = shutil.which(command) # noqa
+        print(f"{command} is {_is_exec}")
+        return 
+
+    found = False
+    paths = os.environ["PATH"].split(":")
+    # print(f"Paths = {paths}")
+    for str_path in paths:
+        file_exists = Path(str_path).joinpath("command").exists()
+        if file_exists:
+            is_exec: str | None = shutil.which(command) # noqa
+            if is_exec is not None:
+                print(f"{command} is {is_exec}")
+                found = True
+                break
+    if not found:
+        print(f"{command}: not found")
+    
+
+def run_command(user_input: str, command: str):
+    args = user_input.lstrip(command).lstrip(" ")
+    args_list = tokenize_quote(args)
+                    
+    paths = os.environ["PATH"].split(":")
+    found = False
+    for path in paths:
+        file_exists = Path(f"{path}/{command}").exists()
+        if file_exists:
+            exec_path: str | None = shutil.which(command)
+            if exec_path is not None:
+                found = True
+                _ = subprocess.run([command] + args_list)
+                break
+
+    if not found:
+        print(f"{command}: command not found") 
+
+
 def parse_quoted_str(text: str, keep_quote: bool = False) -> str:
     current = ""
     last_char_space = False
@@ -110,6 +169,26 @@ def main():
         _ = sys.stdout.write("$ ")
         user_input = input()
         
+        if user_input.startswith("'") or user_input.startswith('"'):
+            args_list = tokenize_quote(user_input)    
+            command, *args = args_list
+            
+            paths = os.environ["PATH"].split(":")
+            found = False
+            for path in paths:
+                file_exists = Path(f"{path}/{command}").exists()
+                if file_exists:
+                    exec_path: str | None = shutil.which(command)
+                    if exec_path is not None:
+                        found = True
+                        _ = subprocess.run([command] + args)
+                        break
+                
+            if not found:
+                print(f"{command}: command not found") 
+            
+            continue
+            
         match user_input.split():
             case ["exit"]:
                 exit()
@@ -118,60 +197,14 @@ def main():
             case ["pwd"]:
                 print(f"{os.getcwd()}")
             case ["cd", directory]:
-                if directory.startswith("~"):
-                    directory = directory.replace("~", os.environ["HOME"])
-                try:
-                    os.chdir(directory)
-                except FileNotFoundError:
-                    print(f"cd: {directory}: No such file or directory")
-                    
+                cd(directory)
             case ["echo", *_]:
-                text = user_input.lstrip("echo").lstrip()
-                print(parse_quoted_str(text))                
+                echo(user_input)         
             case ["type", command]:
-                if command in BUILTIN_CMDS:
-                    print(f"{command} is a shell builtin")
-                    continue 
-                    
-                # Dirty patch until i can figure out why path is corrupted for MG5 Tets
-                if command in ["cat", "cp", "mkdir", "my_exe"]:
-                    is_exec: str | None = shutil.which(command) # noqa
-                    print(f"{command} is {is_exec}")
-                    continue 
-
-                found = False
-                paths = os.environ["PATH"].split(":")
-                # print(f"Paths = {paths}")
-                for str_path in paths:
-                    file_exists = Path(str_path).joinpath("command").exists()
-                    if file_exists:
-                        is_exec: str | None = shutil.which(command) # noqa
-                        if is_exec is not None:
-                            print(f"{command} is {is_exec}")
-                            found = True
-                            break
-                if not found:
-                    print(f"{command}: not found")
+                _type(command)
             # this executes executes available on the system
-            case [command, *args]:
-                args = user_input.lstrip(command).lstrip(" ")
-                args_list = tokenize_quote(args)
-                                
-                paths = os.environ["PATH"].split(":")
-                found = False
-                for path in paths:
-                    file_exists = Path(f"{path}/{command}").exists()
-                    if file_exists:
-                        exec_path: str | None = shutil.which(command)
-                        if exec_path is not None:
-                            found = True
-                            _ = subprocess.run([command] + args_list)
-                            break
-                        else:
-                            continue
-                    
-                if not found:
-                    print(f"{command}: command not found")           
+            case [command, *_]:
+                run_command(user_input, command)          
             case _:
                 print(f"{user_input}: command not found")
 
